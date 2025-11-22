e0ff213d3a69820b520dabee6d258d0902096095
RiyaRani23 <riyarani1404@gmail.com>
2025-11-22 11:25:07 +0530

    Nested Exports

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
  const { calculateSum, x } = require('./calculate/sum'); // CommonJS require statement
+ const { product } = require('./calculate/multiply');
 
 // ES6 import statement
 // import { calculateSum, x } from './sum.js';
 
 console.log("Sum is:", calculateSum(5, 10));
-console.log(x);
+console.log("Product is:", product(5, 10));
 
 
