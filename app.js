37550d0f72068a03650e7a5173a42e26daaa42ce
RiyaRani23 <riyarani1404@gmail.com>
2025-11-22 09:18:03 +0530

    Revising NodeJs

---
+++ app.js
@@ -0,0 +1,17 @@
+var name = "Riya Rani";
+
+var a = 10;
+var b = 20;
+
+// console.log(name);
+// console.log(a + b);
+
+// console.log(window); // In browser, 'window' is the global object
+
+//  console.log(global); // In Node.js, 'global' is the global object
+
+ console.log(this); //empty object in Node.js module
+
+ console.log(globalThis);
+ console.log(globalThis === global); // true in Node.js
+
