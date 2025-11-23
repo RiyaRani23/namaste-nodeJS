da1858942eb7f7b4bc6146b5cfe04f373ef07807
RiyaRani23 <riyarani1404@gmail.com>
2025-11-23 10:11:10 +0530

    SetTimeOut zero second

--- Season1_Basics/setTimeoutZero.js
+++ Season1_Basics/setTimeoutZero.js
@@ -3,6 +3,12 @@ console.log("Hello world!");
 var a = 123456;
 var b = 654321;
 
+// This will be executed after the main thread is free or This callback
+// will be executed when the call stack is empty
+setTimeout(() => {
+    console.log("Call me after zero seconds");
+}, 0);
+
 setTimeout(() => {
     console.log("Call me after 3 seconds");
 }, 3000);
