91bf301187e79c0cb9d46ca31fe56bf73eaa6dab
RiyaRani23 <riyarani1404@gmail.com>
2025-11-24 09:40:21 +0530

    Giving more than 4 task to 4 uv thread Pool

--- Season1_Basics/thread-pool.js
+++ Season1_Basics/thread-pool.js
@@ -16,3 +16,8 @@ crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
 crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
     console.log("PBKDF2 4 completed");
 });
+
+// 5th and more will be queued in the thread pool and will wait for one of the above to complete
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+    console.log("PBKDF2 5 completed");
+});
