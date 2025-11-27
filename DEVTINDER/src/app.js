adf77e99de926a5ec7caa0d3b7e0c2c24e1d7d11
RiyaRani23 <riyarani1404@gmail.com>
2025-11-27 20:41:22 +0530

    nodemon

---
+++ DEVTINDER/src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use("home",(req, res) => {
+    res.send("Hello, DevTinder!" ); 
+})
+
+app.use("/hello", (req, res) => {
+    res.send("Hello, Hello ,Hello!" ); 
+})
+
+app.use("/test",(req, res) => {
+    res.send("Hello, Test " ); 
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
