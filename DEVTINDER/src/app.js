57676d9e271297990a50560e144a85388de2fe14
RiyaRani23 <riyarani1404@gmail.com>
2025-11-28 18:07:27 +0530

    Shortcut routes

--- DEVTINDER/src/app.js
+++ DEVTINDER/src/app.js
@@ -2,23 +2,10 @@ const express = require("express");
 const app = express();
 
 // This route handles GET requests to /user
-app.get("/user", (req, res) => {
+app.get(/fly*/, (req, res) => {
   res.send({firstName: "Riya", lastNmae:"Rani"}); 
 });
 
-app.post("/user", (req, res) => {
-    // Saving data to the database 
-  res.send(" Data successfully saved to the database! "); 
-});
-
-app.delete("/user", (req, res) => {
-    // Deleting data from the database 
-  res.send(" Data successfully deleted ");
-});
-
-app.use("/test",(req, res) => {
-    res.send(" Test " ); 
-})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
