f246f3e3a7684aabbecead96aa495f5a85786ad8
RiyaRani23 <riyarani1404@gmail.com>
2025-11-28 20:29:53 +0530

    Sending Response

--- DEVTINDER/src/app.js
+++ DEVTINDER/src/app.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
 
-// Dynamic Route Parameters means we can define variables in the route path
-// Example: /user/:userId/:name/:password 
-// This is useful for creating routes that can handle different inputs without defining
-//  separate routes for each possible value.
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params); // Logs the route parameters
-  res.send({firstName: "Riya", lastNmae:"Rani"}); 
-});
-
-
+app.use("/user",
+    (req, res, next) => {
+        console.log("Handling the router user!!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling the router user 2 !!");
+        // res.send("2nd Response!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling the router user!!");
+        res.send("Final Response!");
+    },
+)
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
