a0a573d7e3c2d34963bd4a5963a389996bd7a7d9
RiyaRani23 <riyarani1404@gmail.com>
2025-12-01 18:53:01 +0530

    Storing document in database

--- DEVTINDER/src/app.js
+++ DEVTINDER/src/app.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
+app.post("/signup", async (req, res) => {
+  // Creating a new instance of User model
+   const user = new User( {
+    firstName: "Aarav",
+    lastName: "Kumar",
+    emailId: "aarav@234.com",
+    password: "aarav123",
+    
+   });
 
+   try{
+       await user.save();
+       res.send("User added Successfully");
+   }
+   catch(err){
+       res.status(400).send("Error creating user:" + err.message);
+   }
+  
+})
 connectDB()
     .then(() => {
         console.log("Database connected successfully");
--- DEVTINDER/src/config/database.js
+++ DEVTINDER/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://riyarani23:Riya123@web-cluster23.qihgb1i.mongodb.net/?appName=web-cluster23"
+        "mongodb+srv://riyarani23:Riya123@web-cluster23.qihgb1i.mongodb.net/DEVTINDER?appName=web-cluster23/"
     );
 };
 
