1daf9952865d73fea4ad578fe675a3af0dd954e1
RiyaRani23 <riyarani1404@gmail.com>
2025-12-03 12:14:20 +0530

    Adding Validator in signup

---
+++ DEVTINDER/src/utils/validation.js
@@ -0,0 +1,16 @@
+const validator = require('validator');
+
+const validateSignUpData = (req) => {
+    const {firstName, lastName, emailId, password} = req.body;
+
+    if(!firstName || typeof firstName !== 'string' || !lastName || typeof lastName !== 'string') {
+        throw new Error("First name and Last name are required and must be strings");
+    } else if(!emailId || !validator.isEmail(emailId)) {
+        throw new Error("A valid email address is required");
+    } else if(!password || !validator.isStrongPassword(password)) {
+        throw new Error("A strong password is required");
+    }
+
+module.exports = {
+    validateSignUpData
+}
